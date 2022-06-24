a6513fb65c2bc0c0abd92b21bee403d4cba3b9e6
0xMRTT <0xMRTT@tuta.io>
2022-06-24 12:36:48 +0200

    Use sql type time

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,10 @@
+use diesel::sql_types::Time;
+
 #[derive(Queryable)]
 pub struct Mqtt {
     pub id: i32,
     pub topic: String,
     pub payload: String,
-    pub time: String,
+    pub time: Time,
     pub qos: i32,
 }
