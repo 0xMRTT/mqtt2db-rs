c5c9417d7a75fb29e2d870f1e51cf507208eb27e
0xMRTT <0xMRTT@tuta.io>
2022-06-24 10:42:38 +0200

    Add subscribe

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use paho_mqtt::DeliveryToken;
 use std::process;
 /// Simple program to greet a person
 use clap::{Parser, Subcommand};
+extern crate redis;
+use futures::executor::block_on;
+use std::time::Duration;
+use futures::StreamExt;
+
+const TOPICS: &[&str] = &["test", "hello"];
+const QOS: &[i32] = &[1, 1];
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -30,6 +37,12 @@ struct Args {
         #[clap(value_parser)]
         qos: i32,
     },
+    Subscribe{
+        #[clap(value_parser)]
+        topic: String,
+        #[clap(value_parser)]
+        qos: i32,
+    },
  }
 
 fn publish(msg:mqtt::Message, client:&mqtt::AsyncClient) -> DeliveryToken{
@@ -41,15 +54,17 @@ fn main() {
 
     let url = args.url;
 
-    let client = mqtt::AsyncClient::new(url.clone()).unwrap();
+    let create_opts = mqtt::CreateOptionsBuilder::new()
+        .server_uri(url.clone())
+        .client_id("rust_async_subscribe")
+        .finalize();
 
-    if let Err(e) = client.connect(mqtt::ConnectOptions::new()).wait() {
-        println!("Unable to connect:\n\t{:?}", e);
+    let mut client = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
+        println!("Error creating the client: {:?}", e);
         process::exit(1);
-    }
+    });
 
-    println!("Connecting to {} ({})", url.clone(), client.client_id());
-
+    
 
     match &args.command {
         Commands::Publish { topic, payload, qos } => {
@@ -57,6 +72,57 @@ fn main() {
             let token = publish(msg, &client);
             if let Err(e) = token.wait() {
                 println!("Error sending message: {:?}", e);
+            }
+        },
+        Commands::Subscribe { topic, qos } => {
+            if let Err(err) = block_on(async {
+                // Get message stream before connecting.
+                let mut strm = client.get_stream(25);
+        
+                // Define the set of options for the connection
+                let lwt = mqtt::Message::new("test", "Async subscriber lost connection", mqtt::QOS_1);
+        
+                let conn_opts = mqtt::ConnectOptionsBuilder::new()
+                    .keep_alive_interval(Duration::from_secs(30))
+                    .mqtt_version(mqtt::MQTT_VERSION_3_1_1)
+                    .clean_session(false)
+                    .will_message(lwt)
+                    .finalize();
+        
+                // Make the connection to the broker
+                println!("Connecting to the MQTT server...");
+                client.connect(conn_opts).await?;
+        
+                println!("Subscribing to topic: {:?}", topic);
+                client.subscribe(topic, qos.clone()).await?;
+        
+                // Just loop on incoming messages.
+                println!("Waiting for messages...");
+        
+                // Note that we're not providing a way to cleanly shut down and
+                // disconnect. Therefore, when you kill this app (with a ^C or
+                // whatever) the server will get an unexpected drop and then
+                // should emit the LWT message.
+        
+                while let Some(msg_opt) = strm.next().await {
+                    if let Some(msg) = msg_opt {
+                        println!("{}", msg);
+                    }
+                    else {
+                        // A "None" means we were disconnected. Try to reconnect...
+                        println!("Lost connection. Attempting reconnect.");
+                        while let Err(err) = client.reconnect().await {
+                            println!("Error reconnecting: {}", err);
+                            // For tokio use: tokio::time::delay_for()
+                            async_std::task::sleep(Duration::from_millis(1000)).await;
+                        }
+                    }
+                }
+        
+                // Explicit return type for the async block
+                Ok::<(), mqtt::Error>(())
+            }) {
+                eprintln!("{}", err);
             }
         }
     }
